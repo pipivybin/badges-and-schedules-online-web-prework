4a7f4f8c7856b815f93dedc55f263368f0b082a5
Ming Duan <11203duan@gmail.com>
2019-02-02 20:08:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,9 @@ end
 def batch_badge_creator(array)
   array.collect {|name| "Hello, my name is #{name}."}
 end
+
+def assign_rooms(array)
+  arr = []
+  array.each_with_index {|name, x| arr << "Hello, #{name}! You'll be assigned to room #{x}!}
+  arr
+end
